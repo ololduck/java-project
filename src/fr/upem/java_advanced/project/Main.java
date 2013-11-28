b982ca2cc714d38d5715eb929c02bc354007df90
Paul Ollivier <contact@paulollivier.fr>
2013-11-28 12:38:11 +0100

    Initial Commit

---
+++ src/fr/upem/java_advanced/project/Main.java
@@ -0,0 +1,11 @@
+package fr.upem.java_advanced.project;
+
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+
+
+public class Main {
+
+	JSAP jsap = new JSAP();
+	FlaggedOption hello = new FlaggedOption(null);
+}
