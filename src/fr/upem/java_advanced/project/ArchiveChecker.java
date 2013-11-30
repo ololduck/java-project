1ceca4cb6a6112f847c7c9a188a94bb3a6bf959f
Paul Ollivier <contact@paulollivier.fr>
2013-11-30 10:19:40 +0100

    added vendor lib

---
+++ src/fr/upem/java_advanced/project/ArchiveChecker.java
@@ -0,0 +1,17 @@
+package fr.upem.java_advanced.project;
+
+import java.nio.file.Path;
+
+public class ArchiveChecker {
+
+	/**
+	 * Performs a check on a file
+	 * @param Path the path to the file to check
+	 * @return true, if is a zip file
+	 */
+	public static boolean isZipArchive(Path p) {
+		
+		return false;
+	}
+	
+}
