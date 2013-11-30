238709c8c98599bd0de2a04e14925b35974f26e3
Paul Ollivier <contact@paulollivier.fr>
2013-11-30 10:59:16 +0100

    Added utility funciton to check zip file

--- src/fr/upem/java_advanced/project/ArchiveChecker.java
+++ src/fr/upem/java_advanced/project/ArchiveChecker.java
@@ -1,17 +1,33 @@
 package fr.upem.java_advanced.project;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 public class ArchiveChecker {
 
 	/**
 	 * Performs a check on a file
-	 * @param Path the path to the file to check
+	 * 
+	 * @param Path
+	 *            the path to the file to check
 	 * @return true, if is a zip file
+	 * @throws IOException
 	 */
-	public static boolean isZipArchive(Path p) {
-		
+	public static boolean isZipArchive(Path p) throws IOException {
+		ZipFile zip = null;
+		try {
+			zip = new ZipFile(p.toFile());
+		} catch (ZipException e) {
+			return false;
+		} finally {
+			if (zip != null) {
+				zip.close();
+				return true;
+			}
+		}
 		return false;
 	}
-	
+
 }
